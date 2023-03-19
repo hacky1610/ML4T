463714159f310b331be01d0742684d20743af415
Daniel <hacky1610@gmx.de>
2023-03-19 20:02:37 +0100

    Install single pips

--- 22_deep_reinforcement_learning/trading_env.py
+++ 22_deep_reinforcement_learning/trading_env.py
@@ -239,8 +239,8 @@ class TradingEnvironment(gym.Env):
                                           trading_cost_bps=self.trading_cost_bps,
                                           time_cost_bps=self.time_cost_bps)
         self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(self.data_source.min_values,
-                                            self.data_source.max_values)
+        self.observation_space = spaces.Box(self.data_source.min_values.to_numpy(),
+                                            self.data_source.max_values.to_numpy())
         self.reset()
 
     def seed(self, seed=None):
